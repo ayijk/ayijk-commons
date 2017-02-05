3499f5bb419823ef9b17985f6f82cba1a8e38219
ayijk <contact@ayijk.com>
2017-02-05 15:41:19 +0900

    [Uni | Multi]VariateGenerative interface

---
+++ src/main/kotlin/com/ayijk/commons/math/gen/MultiVariateGenerative.kt
@@ -0,0 +1,8 @@
+package com.ayijk.commons.math.gen
+
+/**
+ * Created by ayijk on 2017/02/05.
+ */
+interface MultiVariateGenerative {
+  fun generateArray(): DoubleArray
+}
---
+++ src/main/kotlin/com/ayijk/commons/math/gen/UniVariateGenerative.kt
@@ -0,0 +1,8 @@
+package com.ayijk.commons.math.gen
+
+/**
+ * Created by ayijk on 2017/02/05.
+ */
+interface UniVariateGenerative {
+  fun generate(): Double
+}
